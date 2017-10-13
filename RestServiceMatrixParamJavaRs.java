f5f450d2976104913fecf93855d6b2b69c946ac3
Swapna-WiproRestful <32758455+Swapna-WiproRestful@users.noreply.github.com>
2017-10-13 10:18:32 +0530

    restpathannotations
    queryparam and matrix param

---
+++ RestServiceMatrixParamJavaRs.java
@@ -0,0 +1,22 @@
+package matrixparamexample;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+ 
+@Path("/customers")
+public class RestServiceMatrixParamJavaRs{
+    
+    @GET
+    @Produces("text/html")
+    public Response getResultByPassingValue(
+                    @MatrixParam("nameKey") String name,
+                    @MatrixParam("countryKey") String country) {
+        
+        String output = "Customer name - "+name+", Country - "+country+"";
+        return Response.status(200).entity(output).build();
+ 
+    }
+}
